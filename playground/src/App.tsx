5aadc879e0c489946fc5de408a534ed643071800
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-04-11 16:43:52 +0330

    feat: add flatifycss and react-flatifycss basic demo in playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,7 +1,39 @@
+import { Badge } from "react-flatifycss";
 import { LinkParser } from "../../src";
 
 function App() {
-  return <LinkParser />;
+  return (
+    <div className="card">
+      <p>
+        <LinkParser
+          watchers={[
+            {
+              type: "startsWith",
+              watchFor: "#",
+              render: (text) => <Badge theme="orange">{text}</Badge>,
+            },
+            {
+              type: "startsWith",
+              watchFor: "@",
+              render: (text) => <Badge theme="blue">{text}</Badge>,
+            },
+            {
+              watchFor: "link",
+              render: (url: string) => (
+                <a href={url} target="_blank" rel="noreferrer noopener">
+                  {url}
+                </a>
+              ),
+            },
+          ]}
+        >
+          How happy happy.com I am to be gone! Best of #friends what is the heart of man! https://happy.com To forsake
+          you, #whom I love so much, from whom I was #inseparable and be happy! I @know you will @forgive me for it.
+          Were not my other #associations so chosen by Fate as to make a @heart like mine uneasy?
+        </LinkParser>
+      </p>
+    </div>
+  );
 }
 
 export default App;
