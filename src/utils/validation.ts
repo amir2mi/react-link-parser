d0e37f51762cfdc3322cf2c0f143c538fa69d43e
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-04-12 02:32:28 +0330

    fix: rename isValidUrl file and add email as well

---
+++ src/utils/validation.ts
@@ -0,0 +1,19 @@
+export function isValidUrl(string: string) {
+  // Regex Source: https://www.freecodecamp.org/news/check-if-a-javascript-string-is-a-url/
+  const urlPattern = new RegExp(
+    "^(https?:\\/\\/)" + // validate protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return urlPattern.test(string);
+}
+
+export function isValidEmail(string: string) {
+  // Regex Source: https://www.dotnettricks.com/learn/javascript/javascript-email-address-validation-using-regular-expression
+  var emailPattern = new RegExp(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/);
+  return emailPattern.test(string);
+}
