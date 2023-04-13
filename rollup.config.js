9e0554af1d625b21f4008e5efa1a5b3b3aa44a6a
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-04-13 17:17:43 +0330

    fix: removed playground rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -6,11 +6,6 @@ export default [
   {
     input: "src/index.ts",
     output: [
-      {
-        file: "playground/src/component-lib/index.js",
-        format: "esm",
-        banner: "/* eslint-disable */",
-      },
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "esm" },
     ],
