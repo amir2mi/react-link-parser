ffb4bdd4626afa38fddc26a2f83e968f28c6c14d
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-04-16 22:53:00 +0330

    fix: change the base url of website

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const config = {
+    base: "/react-link-parser/",
     plugins: [react()],
     define: {} as any,
   };
