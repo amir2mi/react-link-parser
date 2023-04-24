cfcc2d383ed252ee66b8bc5dd0369f986feae148
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2023-04-25 01:04:10 +0330

    feat: add Arabic regex to url validation util

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -2,7 +2,7 @@ export function isValidUrl(string: string) {
   // Regex Source: https://www.freecodecamp.org/news/check-if-a-javascript-string-is-a-url/
   const urlPattern = new RegExp(
     "^(https?:\\/\\/)" + // validate protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+      "((([a-z\u0600-\u065F\u066E-\u06D5\\d]([a-z\u0600-\u065F\u066E-\u06D5\\d-]*[a-z\u0600-\u065F\u066E-\u06D5\\d])*)\\.)+[a-z\u0600-\u065F\u066E-\u06D5]{2,}|" + // validate domain name
       "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
       "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
       "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
